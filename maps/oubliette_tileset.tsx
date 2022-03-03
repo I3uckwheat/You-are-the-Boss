b0507d620295f2b07b8c12e484f4cf7726dd7afa
Briggs Elsperger <briggse@protonmail.com>
2022-03-03 07:08:58 -0700

    adds base-map and start phaser setup

---
+++ maps/oubliette_tileset.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="oubliette_tileset" tilewidth="16" tileheight="16" tilecount="64" columns="8">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <image source="../assets/tiles/oubliette_tileset.png" width="128" height="128"/>
+</tileset>
